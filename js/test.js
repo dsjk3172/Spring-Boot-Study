e024f72d432404c9ff3362ee80c2c8cb45c272d6
Sihyun Lee <shl2023@itier.co.kr>
2023-04-18 14:39:19 +0900

    2023.04.18 - question_3

--- js/test.js
+++ js/test.js
@@ -1,6 +1,6 @@
 function question_2_DialogBox() {
     var i = 0;
-    var end = document.getElementById('question_2').value;
+    var end = document.getElementById('txtQuestion_2').value;
     var sum = 0;
     for(i; i <= end; i++){
         sum += i
@@ -9,6 +9,6 @@ function question_2_DialogBox() {
 }
 
 function change() {
-var str = document.getElementById("text");
+var str = document.getElementById("txtQuestion_3");
 		str.innerHTML = "이 문장으로 바뀌었습니다!";
 }
